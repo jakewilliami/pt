830790a73f8e09522b7a619d480afa9e7cc64eed
Jake Ireland <jakewilliami@icloud.com>
2026-07-23 23:33:52 +1200

    Expose dates submodule to library!  v1.1.0
    Date utilities such as `dates::last_day_of_month` may be convenient
    downstream for users of this library.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod dates;
 pub mod penultimate;
 
 pub use penultimate::penultimate_day_of_month;
--- src/penultimate.rs
+++ src/penultimate.rs
@@ -1,13 +1,11 @@
+use crate::dates::last_day_of_month;
 use chrono::{Datelike, Duration, NaiveDate, Weekday};
-
-#[path = "./dates.rs"]
-mod dates;
 
 pub fn penultimate_day_of_month(d: NaiveDate, day: Weekday) -> NaiveDate {
     // Implementation stolen from PenultimateDays.jl:
     // https://github.com/jakewilliami/PenultimateDays.jl/blob/2e3232f5dcf768ca8a1b2a9618a006b839411090/src/PenultimateDays.jl#L43-L46
     let i = day.number_from_monday() as i64;
-    let ld_date = dates::last_day_of_month(d);
+    let ld_date = last_day_of_month(d);
     let ld_i = ld_date.weekday().number_from_monday() as i64;
     let ld_day = 7 * ((ld_i < i) as i64) - i + ld_i + 1;
     ld_date
